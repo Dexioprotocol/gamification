c8b2c0e7880af20d86c1bc36e73124f3121cd857
Michael <michael@sensemaking.solutions>
2023-03-06 09:09:48 +0700

    add logging for message send/recieve

--- src/amqp-connector.js
+++ src/amqp-connector.js
@@ -1,5 +1,6 @@
 const logger = require('winston');
 const amqp = require('amqplib');
+
 class AmqpConnector {
   constructor(host, app) {
     this.host = host;
@@ -7,7 +8,7 @@ class AmqpConnector {
   }
 
   async connect() {
-    while(!this.connection) {
+    while (!this.connection) {
       try {
         this.connection = await amqp.connect('amqp://' + this.host);
         logger.info('RabbitMQ connected');
@@ -20,7 +21,7 @@ class AmqpConnector {
         logger.warn(error);
         logger.log('RabbitMQ connection failed. Reconnecting in 1 second ...');
         var waitTill = new Date(new Date().getTime() + 1000);
-        while(waitTill > new Date()){
+        while (waitTill > new Date()) {
           // busy waiting
         }
       }
@@ -32,6 +33,7 @@ class AmqpConnector {
       await this.channel.assertQueue(queue, {
         durable: true
       });
+      logger.info('sendToQueue: ', message);
       await this.channel.sendToQueue(queue, Buffer.from(message));
     } catch (error) {
       logger.warn(error);
@@ -43,6 +45,7 @@ class AmqpConnector {
       await this.channel.assertExchange(exchange, 'direct', {
         durable: true
       });
+      logger.info('sendToExchange: ', message);
       await this.channel.publish(exchange, routingKey, Buffer.from(message));
     } catch (error) {
       logger.warn(error);
@@ -56,6 +59,7 @@ class AmqpConnector {
       });
       this.channel.consume(queue,
         (msg) => {
+          logger.info('receiveFromQueue', msg.content);
           this.app.service('events').create(JSON.parse(msg.content));
         }, {
           noAck: true
